ab46905fa5c5fac3bfc34ac78b3f1f830396ec1e
gentrit.osmani <TheworldisBig99>
2021-04-07 18:01:12 +0200

    h

--- src/kahoot.java
+++ src/kahoot.java
@@ -1,7 +1,7 @@
 
 public class kahoot {
     public static void main(String args[]) {
-        String s = new String("5");
-        System.out.println(1+10+s+1+11);
+        String s = new String("2");
+        System.out.println(1+10+s+1+11+33);
     }
 }
